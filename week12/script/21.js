4cfa247c270d5ce16b05ca134d780e3cc65f046e
RootUnvalue <mls1218@naver.com>
2023-06-02 10:09:04 +0900

    updates

--- week12/script/21.js
+++ week12/script/21.js
@@ -5,3 +5,7 @@
 function cb(cl) {
     document.body.style.backgroundColor = cl;
 }
+
+function reset(){
+    document.body.style.backgroundColor = 'white';
+}
--- week13/script/21.js
+++ week13/script/21.js
@@ -1,19 +1,24 @@
 var imgs = document.getElementsByTagName("img");
+var imgsf;
 
 imgs[0].ondragstart = function (e) {
     e.dataTransfer.setData("text", e.target.id);
+    imgsf = '"../week12/src/봇치.webp"';
 }
 
 imgs[1].ondragstart = function (e) {
     e.dataTransfer.setData("text", e.target.id);
+    imgsf = '"../week12/src/니지카.webp"';
 }
 
 imgs[2].ondragstart = function (e) {
     e.dataTransfer.setData("text", e.target.id);
+    imgsf = '"../week12/src/료.webp"';
 }
 
 imgs[3].ondragstart = function (e) {
     e.dataTransfer.setData("text", e.target.id);
+    imgsf = '"../week12/src/키타.webp"';
 }
 
 var dropfeild = document.getElementById("dropfeild");
@@ -22,13 +27,18 @@ dropfeild.ondragover = function (e) {
     e.preventDefault();
     e.target.style.background = 'gray';
 }
+var backgroundst = document.getElementById("dropfeild").style;
 
 dropfeild.ondrop = function (e){
     e.preventDefault();
     e.target.style.background = 'transparent';
 
     var file_name = e.dataTransfer.getData("text");
-    document.getElementById("name").innerText = "파일 이름 : " + file_name;
+    backgroundst.backgroundImage = 'url(' + imgsf + ')';
+    backgroundst.backgroundRepeat = 'no-repeat';
+    backgroundst.backgroundPosition = 'center';
+    backgroundst.backgroundSize = 'auto';
+
 }
 
 
